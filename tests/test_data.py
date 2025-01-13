1f453e241f26624b8e59f715f4da46281de885b1
Oskar Bohn Lassen <obola@dtu.dk>
2025-01-13 14:12:07 +0100

    finalizing test implementation

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,10 @@
 import os.path as osp
 import pickle
 import torch
+import pytest
+import os
 
+@pytest.mark.skipif(not os.path.exists("data/raw/sioux_falls_simulation_24_zones_OD_2K"), reason="Data files not found")
 class TestSiouxFalls24Zones:
 
     def test_raw_data(self):
@@ -9,15 +12,18 @@ class TestSiouxFalls24Zones:
         
         raw_data_path = "data/raw/sioux_falls_simulation_24_zones_OD_2K"
 
+        all_graphs = []
         # Preprocess the data
         for split in ['train', 'val', 'test']:
             with open(osp.join(raw_data_path, f'{split}.pickle'), 'rb') as f:
                 graphs = pickle.load(f)
-
+                all_graphs.extend(graphs)
             assert graphs[0].x.shape == (24, 24)
             assert graphs[0].edge_attr.shape == (228,) ### raw data should be changed to 76 x 3 shape
             assert graphs[0].y.shape == (76,)
             assert graphs[0].edge_weight.shape == (76,)
+        
+        assert len(all_graphs) == 2000 ### Not sure I like to do this test if the number of graphs is not fixed
 
     def test_preprocessed_data(self):
         '''Test whether the preprocessed data is correct.'''
